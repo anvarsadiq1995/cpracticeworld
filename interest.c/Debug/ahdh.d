54c8784d46cd0ccb8eb49ad331296299d9cc68a1
anvarsadiq1995 <anvarsadiq70@gmail.com>
2019-11-11 17:48:59 -0500

    compound interest

---
+++ interest.c/Debug/ahdh.d
@@ -0,0 +1,1 @@
+ahdh.o: ../ahdh.c
---
+++ interest.c/ahdh.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int   main(){
+	 float a = 10000;
+	 float b = 0.06;
+	 float c = 30;
+	 float um ;
+		 for ( int i = 1; i <= c ; i++)
+	 {
+		 um = (a + (b * a));
+	a = um;
+}
+
+	 printf  ("sum = %f ", um);
+	 return 0;
+}
